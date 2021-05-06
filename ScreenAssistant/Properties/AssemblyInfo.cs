12272eee2a4d71f68a72bb2f3369defc32d2044e
OsROMS <33641601+OsROMS@users.noreply.github.com>
2021-05-06 15:11:13 +0100

    Change Assembly Version

--- ScreenAssistant/Properties/AssemblyInfo.cs
+++ ScreenAssistant/Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.100.0")]
-[assembly: AssemblyFileVersion("1.3.100.0")]
+[assembly: AssemblyVersion("1.3.999.9")]
+[assembly: AssemblyFileVersion("1.3.999.9")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("00000000-0000-0000-0000-000000000000")]
